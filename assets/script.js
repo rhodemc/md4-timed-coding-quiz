6579fa35867dd92e03b4108b928e2424dc6cdf0b
rhodemc <rhodemc@gmail.com>
2023-02-20 21:11:34 -0800

    This update is mostly focused on JS

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,88 @@
-let myQuestionsArr = document.querySelector("#questionsArr");
 
-let myQuestions = [{
+const quizIntro = document.querySelector('.quiz-intro');
+const startQuizBtn = document.querySelector('#startQuiz');
+const quizQuestions = document.querySelector('.quiz-questions');
+const questionsArr = document.querySelector('#questionsArr');
+const timer = document.querySelector('#timer');
+const answerBtn = document.querySelectorAll('.answerBtn');
+const quizEnd = document.querySelector('.quiz-end');
+const finalScore = document.querySelector('#finalScore');
+const submitScoreBtn = document.querySelector('.submit-btn');
+const highScores = document.querySelector('.high-scores');
+
+let processId = 0;
+let timerTime = 75;
+let questionsIndex = 0;
+
+let myQuestions = [
+    {
     question: "What line of code would you use to print to your debugger?",
-    answerOptions: ["console.log()", "boolean", "function", "class",],
+    answerOptions: ["console.log()", "boolean", "function", "class"],
     answer: "console.log()",
-}, {
+    }, 
+    {
     question: "What does coercion mean in regards to programming?",
-    answerOptions:["To convert between types", "To eat", "To poop", "To debug",],
+    answerOptions:["To convert between types", "To eat", "To poop", "To debug"],
     answer: "To convert between types",
-}, {
+    }, 
+    {
     question: "Which of the following choices is NOT a for loop clause?",
-    answerOptions:["Initialization", "Conditional Test", "Update", "Water bottle",],
+    answerOptions:["Initialization", "Conditional Test", "Update", "Water bottle"],
     answer: "Water bottle",
-}, {
+    }, 
+    {
     question: "What does append mean in regards to programming?",
-    answerOptions:["To insert content at the end of a specific element", "To round down", "To round up", "To round across",],
+    answerOptions:["To insert content at the end of a specific element", "To round down", "To round up", "To round across"],
     answer: "To insert content at the end of a specific element",
-}];
+    }
+];
 
-for (i = 0; i < myQuestions.length; i++) {
-    console.log(myQuestions[i].question)
-    myQuestionsArr.innerHTML = myQuestions[i].question
-}
+startQuizBtn.addEventListener("click", () => {
+    processId = setInterval(() => {
+        timer.textContent = timerTime--
+    }, 1000);
+        quizIntro.setAttribute("class", "quiz-intro hidden");
+        quizQuestions.setAttribute("class", "quiz-questions visible")
+        getNewQuestion();
+});
+
+function getNewQuestion() {
+    questionsArr.innerHTML = myQuestions[questionsIndex].question
+    for (let i = 0; i < answerBtn.length; i++) {
+        answerBtn[i].textContent = myQuestions[questionsIndex].answerOptions[i];
+        answerBtn[i].addEventListener("click", nextQuestion)  
+    };
+};
+
+function nextQuestion() {
+    checkAnswer(this.textContent)
+    questionsIndex++
+    if (questionsIndex < myQuestions.length) {
+        getNewQuestion();
+    } else {
+        clearInterval(processId);
+        endQuiz();
+    };
+};
+
+function checkAnswer(btnText) {
+    var answer = myQuestions[questionsIndex].answer
+    if (answer === btnText) {
+        alert("Correct!")
+    } else {
+        alert("Wrong!")
+        timerTime = timerTime - 10;
+    };
+};
+
+function endQuiz() {
+    quizEnd.setAttribute("class", "quiz-end visible");
+    quizQuestions.setAttribute("class", "quiz-questions hidden");
+    finalScore.innerHTML = timerTime + 1;
+};
+
+submitScoreBtn.addEventListener("click", function() {
+    localStorage.setItem('highScore', finalScore.innerHTML);
+    quizEnd.setAttribute("class", "quiz-end hidden");
+    highScores.setAttribute("class", "high-scores visible");
+});
